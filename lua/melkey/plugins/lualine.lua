8a4ad2598ecf7f09912739f55eb5977b705fc3e4
darrius <mohamedtine17@gmail.com>
2023-12-26 09:52:39 +0000

    change style lualine

--- lua/melkey/plugins/lualine.lua
+++ lua/melkey/plugins/lualine.lua
@@ -5,32 +5,77 @@ return {
 		requires = { "nvim-tree/nvim-web-devicons", opt = true },
 		opts = {
 			options = {
-				icons_enabled = true,
 				theme = "catppuccin",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {},
-				always_divide_middle = true,
+				component_separators = "|",
+				section_separators = { left = "", right = "" },
 				globalstatus = true,
+				ignore_focus = {
+					"dapui_watches",
+					"dapui_stacks",
+					"dapui_breakpoints",
+					"dapui_scopes",
+					"dapui_console",
+					"dap-repl",
+				},
 			},
 			sections = {
-				lualine_a = { "mode" },
+				lualine_a = {
+					{ "mode", separator = { left = "" }, right_padding = 2 },
+				},
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { { "%f", color = { gui = "bold" } } },
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = { "fileformat", "filetype" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					"filetype",
+					{
+						function()
+							local icon = require("lazyvim.config").icons.kinds.Copilot
+							local status = require("copilot.api").status.data
+							return icon .. (status.message or "")
+						end,
+						cond = function()
+							if not package.loaded["copilot"] then
+								return
+							end
+							local ok, clients =
+								pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
+							if not ok then
+								return false
+							end
+							return ok and #clients > 0
+						end,
+						color = function()
+							if not package.loaded["copilot"] then
+								return
+							end
+							local Util = require("lazyvim.util")
+							local colors = {
+								[""] = Util.ui.fg("Special"),
+								["Normal"] = Util.ui.fg("Special"),
+								["Warning"] = Util.ui.fg("DiagnosticError"),
+								["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+							}
+							local status = require("copilot.api").status.data
+							return colors[status.status] or colors[""]
+						end,
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = {
+					{
+						function()
+							local loc = require("lualine.components.location")()
+							local sel = require("lualine.components.selectioncount")()
+							if sel ~= "" then
+								loc = loc .. " (" .. sel .. " sel)"
+							end
+							return loc
+						end,
+						separator = { right = "" },
+						left_padding = 2,
+					},
+				},
 			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			extensions = {},
+			extensions = { "neo-tree" },
 		},
 	},
 }
